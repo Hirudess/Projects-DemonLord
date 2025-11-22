66995388f853ba97b9346b9de3307ccd7eb03fe2
gabokun88 <gramadi88@gmail.com>
2025-11-22 23:01:16 +0700

    character inventory and creation

---
+++ Assets/Scripts/Character/CharacterInventoryDataService.cs
@@ -0,0 +1,38 @@
+﻿using FDK.Core;
+
+namespace FDK.Character
+{
+    public interface ICharacterInventoryDataService
+    {
+        CharacterInventoryData CharacterInventoryData { get; }
+
+        void Add(CharacterData characterData);
+        void GetData(CharacterData characterData);
+        void Remove(CharacterData characterData);
+    }
+
+    public class CharacterInventoryDataService : BaseService, ICharacterInventoryDataService
+    {
+        public CharacterInventoryData CharacterInventoryData { get; private set; }
+
+        public CharacterInventoryDataService()
+        {
+
+        }
+
+        public void Add(CharacterData characterData)
+        {
+            CharacterInventoryData.Add(characterData);
+        }
+
+        public void Remove(CharacterData characterData)
+        {
+            CharacterInventoryData.Equals(characterData);
+        }
+
+        public void GetData(CharacterData characterData)
+        {
+            CharacterInventoryData.GetData(characterData);
+        }
+    }
+}
